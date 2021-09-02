03806d9114ecf7dd91ef11f6436c4243d4fab880
heilingyu <62786174+heilingyu@users.noreply.github.com>
2021-09-02 22:59:07 +0800

    Add files via upload

--- degrees_minutes_seconds_to decimals.c
+++ degrees_minutes_seconds_to decimals.c
@@ -3,68 +3,70 @@
 
 int main(void)
 {
-	int ture_longitude = 0;
-	float degree_longitude, minute_longitude, second_longitude, longitude;
-	degree_longitude = 0; minute_longitude = 0; second_longitude = 0; longitude = 0;
-	
-	for (ture_longitude; ture_longitude < 1; ture_longitude * 1)
+	int again = 1;
+	for (again; again ==1; again * 1)
 	{
-		printf("请依次输入所求地点的经度的度、分、秒，并用回车隔开（东经为正，西经为负）:\n");
+		int ture_longitude = 0;
+		float degree_longitude, minute_longitude, second_longitude, longitude;
+		degree_longitude = 0; minute_longitude = 0; second_longitude = 0; longitude = 0;
 
-		scanf_s("%f", &degree_longitude);
-		//printf("%f\n", degree);
+		for (ture_longitude; ture_longitude < 1; ture_longitude * 1)
+		{
+			printf("请依次输入所求地点的经度的度、分、秒，并用回车隔开（东经为正，西经为负）:\n");
 
-		scanf_s("%f", &minute_longitude);
-		//printf("%f\n", minute);
-		float minute_results_longitude = minute_longitude / 60;
+			//输入数据
+			scanf_s("%f", &degree_longitude);
 
-		scanf_s("%f", &second_longitude);
-		//printf("%f\n", second);
-		float second_results_longitude = second_longitude / 3600;
+			scanf_s("%f", &minute_longitude);
+			float minute_results_longitude = minute_longitude / 60;
 
-		longitude = degree_longitude + minute_results_longitude + second_results_longitude + 1 / 1000000;
-		
-		if (longitude > -180 && longitude < 180) {
-			ture_longitude = ture_longitude + 2;
+			scanf_s("%f", &second_longitude);
+			float second_results_longitude = second_longitude / 3600;
+
+			//计算
+			longitude = degree_longitude + minute_results_longitude + second_results_longitude + 1 / 1000000;
+
+			//判断合法性
+			if (longitude > -180 && longitude < 180) {
+				ture_longitude = ture_longitude + 2;
+			}
+			else {
+				system("cls");
+				printf("错了，重来！\n");
+			}
 		}
-		else{
-			system("cls");
-			printf("错了，重来！\n");
+		printf("经度=%f\n", longitude);
+
+		int ture_latitude = 0;
+		float degree_latitude, minute_latitude, second_latitude, latitude;
+		degree_latitude = 0; minute_latitude = 0; second_latitude = 0; latitude = 0;
+		for (ture_latitude; ture_latitude < 1; ture_latitude * 1)
+		{
+			printf("请依次输入所求地点的纬度的度、分、秒，并用回车隔开（北纬为正，南纬为负）:\n");
+
+			scanf_s("%f", &degree_latitude);
+
+			scanf_s("%f", &minute_latitude);
+			float minute_results_latitude = minute_latitude / 60;
+
+			scanf_s("%f", &second_latitude);
+			float second_results__latitude = second_latitude / 3600;
+
+			latitude = degree_latitude + minute_results_latitude + second_results__latitude + 1 / 1000000;
+
+			if (latitude > -90 && latitude < 90) {
+				ture_latitude = ture_latitude + 2;
+			}
+			else {
+				system("cls");
+				printf("请依次输入所求地点的经度的度、分、秒，并用回车隔开（东经为正，西经为负）:\n%f\n%f\n%f\n经度=%f\n", degree_longitude, minute_longitude, second_longitude, longitude);
+				printf("错了，重来！\n");
+			}
 		}
+		printf("纬度=%f\n\n", latitude);
+		printf("是否退出（退出输入0，继续输入1）\n");
+		scanf_s("%d", &again);
 	}
-	printf("经度=%f\n", longitude);
-
-	int ture_latitude = 0;
-	float degree_latitude, minute_latitude, second_latitude, latitude;
-	degree_latitude = 0; minute_latitude = 0; second_latitude = 0; latitude = 0;
-	for (ture_latitude; ture_latitude < 1; ture_latitude * 1)
-	{
-		printf("请依次输入所求地点的纬度的度、分、秒，并用回车隔开（北纬为正，南纬为负）:\n");
-
-		scanf_s("%f", &degree_latitude);
-		//printf("%f\n", degree);
-
-		scanf_s("%f", &minute_latitude);
-		//printf("%f\n", minute);
-		float minute_results_longitude = minute_latitude / 60;
-
-		scanf_s("%f", &second_latitude);
-		//printf("%f\n", second);
-		float second_results__latitude = second_latitude / 3600;
-
-		latitude = degree_latitude + minute_results_longitude + second_results__latitude + 1 / 1000000;
-
-		if (latitude > -90 && latitude < 90) {
-			ture_latitude = ture_latitude + 2;
-		}
-		else {
-			system("cls");
-			printf("请依次输入所求地点的经度的度、分、秒，并用回车隔开（东经为正，西经为负）:\n%f\n%f\n%f\n经度=%f\n", degree_longitude, minute_longitude, second_longitude, longitude);
-			printf("错了，重来！\n");
-		}
-	}
-	printf("纬度=%f\n\n", latitude);
-	
 	system("pause");
 	return 0;
 }
